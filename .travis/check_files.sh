63e9771721cc71368eb87f294d1b2ff79e95f3fb
Aaryn Olsson <aaryn@email.arizona.edu>
2019-10-28 22:52:35 -0700

    Update check_files.sh

--- .travis/check_files.sh
+++ .travis/check_files.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 ret=0
-cd "$(dirname "$0")"
-for f in $(cat required_files.txt); do
-  if [ ! -r $f ]; then
-     >&2 echo "file $f does not exist or is not readable"
+dir=$(dirname "$0")
+if [ ! -f $dir/required_files.txt ]; then
+    echo "dir/required_files.txt not found"
+    exit 1
+fi
+for f in $(cat $dir/required_files.txt); do 
+  filename=$(basename -- "$f")
+  extension="${filename##*.}"
+  extension_upper=$(printf '%s\n' "$extension" | awk '{ print toupper($0) }')
+  filename="${filename%.*}"
+  if [[ ! -r $filename.$extension ]] && [[ ! -r $filename.$extension_upper ]]; then 
+     echo "file $f does not exist or is not readable"
      ret=1
+  else
+    echo "$f exists:"
+    ls -al $f
   fi
 done
 exit $ret
